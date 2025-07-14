5bfdb6a062725a44232592f667aa7bd1342ceb8e
MAYANK JAAT <mayankadsens1@gmail.com>
2025-07-14 10:36:13 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -144,6 +144,18 @@ html_content = """
     </style>
 </head>
 <body>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1162417600616917"
+     crossorigin="anonymous"></script>
+<!-- Player Home 1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1162417600616917"
+     data-ad-slot="6504990098"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     <!-- Main Content -->
     <div class="container">
         <div class="team-spy">Warrior Units Bots</div>
